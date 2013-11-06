16a0804b8bfe96a89cf0d27c66504c0a9bb0e5bc
Rafi Kamal <rafikamal93@gmail.com>
2013-11-06 17:12:42 +0600

    Update 1174 - Commandos.cpp

--- LightOj/Graph Theory/1174 - Commandos.cpp
+++ LightOj/Graph Theory/1174 - Commandos.cpp
@@ -2,12 +2,12 @@
 Algorithm: BFS/DFS
 
 There is an unlimited supply of commando troops for the mission, so we can assign one commando
-for destroying only one building. So, the i'th commando have to:
+for destroying one building. So, the i'th commando have to:
 1. Start from the building s and go to the i'th building
 2. From the i'th building, go to building d
 
-So, we have to calculate (shortest path from u to i) + (shortest path from d to i)
-for all commandos, and find the maximum among them.
+So, we have to calculate (shortest path from s to i) + (shortest path from d to i)
+for all commandos, and find the maximum value among these.
 */
 
 #include <cstdio>
