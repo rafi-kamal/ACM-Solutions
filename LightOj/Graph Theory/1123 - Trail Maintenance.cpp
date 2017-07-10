20d7c8f7884f49aa01b899176d111f8e6b54a028
Anil Kumar Kasaragadda <kasaragaddaanilkumar1998@gmail.com>
2017-07-10 17:27:08 +0530

    Update and rename Light_Oj 1123.cpp to 1123 - Trail Maintenance.cpp

--- LightOj/Graph Theory/1123 - Trail Maintenance.cpp
+++ LightOj/Graph Theory/1123 - Trail Maintenance.cpp
@@ -2,19 +2,19 @@
 Algorithm: Kruskal + DSU
 
 Explanation: Here we have to first check for edge,will it make a single connected component or not.
-			 Once it is connected component then for each edge apply Kruskal Algorithm.
-			 The useful optimization is we have to take only the MST as a graph each time.
-			 (If not understood,there is a clear cut expanation in forum.Have a look at it.) 
+	     Once it is connected component then for each edge apply Kruskal Algorithm.
+	     The useful optimization is we have to take only the MST as a graph each time.
+             (If not understood,there is a clear cut expanation in forum.Have a look at it.) 
 */
 #include<bits/stdc++.h>
 using namespace std;
 #define opt 	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define lli 	long long int
-#define mod		1000000007
+#define mod	1000000007
 #define inf     1000000000000000000
-#define MX1		100005
-#define MX2		10000004
-#define pi		acos(-1)
+#define MX1	100005
+#define MX2	10000004
+#define pi	acos(-1)
 #define pb      push_back
 #define vlli    vector<lli>
 #define plli    vector<pair<lli,lli> >
@@ -99,8 +99,8 @@ int main()
 			s=dsu(g);
 			if(s==1)
 			{
-		 		ok=1;
-		 		initialize();
+		 	     ok=1;
+		 	     initialize();
 			}
 	   	}
 	   	if(ok==1)
@@ -108,8 +108,8 @@ int main()
 	   		sort(all(g));
 	   		min=minimum(g,v);
 	   		g.clear();
-       	    g.assign(v.begin(),v.end());
-        	v.clear();
+       	    		g.assign(v.begin(),v.end());
+        		v.clear();
 	   		printf("%lld\n",min);
 	   	}
 	   	else
